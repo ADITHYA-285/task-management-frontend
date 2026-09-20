1d57baa67c06c7bccea306df9b0032afba8cc1ae
Mohan G <symbolequal@gmail.com>
2026-09-20 11:26:46 +0530

    add route setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import Login from './pages/login';
 import Dashboard from './pages/dashboard';
 import Register from "./pages/register";
 import ProtectedRoute from "./components/protectedroute";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* When user opens /, send them to login */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
 
         <Route path="/login" element={<Login />} />
 
@@ -20,6 +23,7 @@ function App() {
             </ProtectedRoute>
           }
         />
+
         <Route path="/register" element={<Register />} />
 
       </Routes>
@@ -27,5 +31,4 @@ function App() {
   );
 }
 
-
-export default App
+export default App;
